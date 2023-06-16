bc2cbc7dd1ade85b17d32c23c5c9d3211ef3134e
StolenGalaxy <matthew.ridley26@gmail.com>
2023-06-16 15:29:18 +0100

    Update

--- main.py
+++ main.py
@@ -20,26 +20,21 @@ class advertiser:
             "Authorization" : auth
         }
         
-        post(f"https://discord.com/api/v9/channels/{channel}/messages", data=data, headers=headers)
-        return
+        response = post(f"https://discord.com/api/v9/channels/{channel}/messages", data=data, headers=headers)
+        return response.status_code
         
     async def main(self):
         channels = [channel for channel in open(self.channelsDirectory).read().splitlines()]
-        print(channels)
         auths = [auth for auth in open(self.authsDirectory).read().splitlines()]
         
         funcs = [self.sendMessage(channel, choice(auths)) for channel in channels]
-        
-        await asyncio.gather(*funcs)
-        print("Finished sending messages. Waiting.")
+        responses = await asyncio.gather(*funcs)
+        print(f"Sent messages with responses: {responses}")
         await asyncio.sleep(self.delay)
-        print("Finished waiting, restarting.")
         
         
-            
-    
     
 a = advertiser(authsDirectory="auths.txt", messageDirectory="message.txt", channelsDirectory="channels.txt")
 
-
-asyncio.run(a.main())
+while True:
+    asyncio.run(a.main())
