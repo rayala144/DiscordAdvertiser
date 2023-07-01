e2f4685d44236f9cb0a58b13e1d81687a3fc8d17
StolenGalaxy <matthew.ridley26@gmail.com>
2023-07-01 22:56:37 +0100

    Ready for publication

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from time import sleep
 from random import choice
-from requests import post
+import aiohttp
 import asyncio
 
 class advertiser:
@@ -10,7 +9,6 @@ class advertiser:
         self.channelsDirectory = channelsDirectory
         self.delay = delay
         self.message = open(messageDirectory).read()
-        
     
     async def sendMessage(self, channel, auth):
         data = {
@@ -20,14 +18,16 @@ class advertiser:
             "Authorization" : auth
         }
         
-        response = post(f"https://discord.com/api/v9/channels/{channel}/messages", data=data, headers=headers)
-        return response.status_code
+        async with aiohttp.ClientSession() as session:
+            async with session.post(f"https://discord.com/api/v9/channels/{channel}/messages", data=data, headers=headers) as response:
+                return response.status
+        
         
     async def main(self):
         channels = [channel for channel in open(self.channelsDirectory).read().splitlines()]
-        auths = [auth for auth in open(self.authsDirectory).read().splitlines()]
+        account = choice([auth for auth in open(self.authsDirectory).read().splitlines()])
         
-        funcs = [self.sendMessage(channel, choice(auths)) for channel in channels]
+        funcs = [self.sendMessage(channel, account) for channel in channels]
         responses = await asyncio.gather(*funcs)
         print(f"Sent messages with responses: {responses}")
         await asyncio.sleep(self.delay)
